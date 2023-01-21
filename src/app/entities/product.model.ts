8ecc08ccc3d49ebdc37ce9c22dfd09856ee02f94
JimmyVin <jimmy-dragon1@hotmail.com>
2023-01-20 22:18:47 -0500

    delete index.lock

--- src/app/entities/product.model.ts
+++
@@ -1,8 +0,0 @@
-import { CategoryModel } from './category.model';
-export interface ProductModel{
-    id:number
-    title:string
-    price:number
-    description:string
-    category:CategoryModel;
-}
